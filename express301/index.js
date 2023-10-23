0a2796a6d0d81077a22a56c0ffa2a0731ea4c558
jos50275266 <jos50275266@gmail.com>
2023-10-23 20:42:22 +0900

    modification

--- express301/index.js
+++ express301/index.js
@@ -26,6 +26,14 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(validateUser);
 
+// wildcard
+app.param(":id", (req, res, next, id) => {
+  console.log("Param Called: ", id);
+  // req.params.id
+
+  next();
+});
+
 app.post("/process_login", (req, res) => {
   const { password, username } = req.body;
 
@@ -35,12 +43,44 @@ app.post("/process_login", (req, res) => {
   }
 });
 
+// welcome 라우터를, get 메소드를 사용해서 처리하고, 응답으로는 welcome.ejs를 전달해주고, 데이터는 쿠키에서 username을 추출한걸 인자로 전달해줘
 app.get("/welcome", (req, res) => {
   // console.log(req.cookies.username);
 
   res.render("welcome", {
     username: req.cookies.username,
   });
+});
+
+// localhost:3000/user/1
+// localhost:3000/user/2
+// localhost:3000/user/3
+// localhost:3000/user/4 ~ 1,000,000,000
+
+app.get("/users/:id", (req, res) => {
+  console.log("user/:id - ", req.params.id);
+
+  res.send(`<h1>Id IS ${req.params.id}</h1>`);
+});
+
+// Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client
+// app.get("/statement", (req, res, next) => {
+//   res.json({ msg: "test" });
+//   res.send("Hello World");
+// });
+
+app.get("/download", (req, res, next) => {
+  res.download(
+    path.join(__dirname, "images/panda.png"),
+    "cozyserver.png",
+    (error) => {
+      if (error) {
+        if (!res.headersSent) {
+          res.redirect("/download/error");
+        }
+      }
+    }
+  );
 });
 
 app.post("/", (req, res) => {
